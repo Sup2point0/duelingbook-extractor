464d3f4a6fcdaf93532ebc9002abaf1210c641ab
Sup#2.0 <sup2point0@gmail.com>
2025-12-23 16:46:53 +0000

    dev - restructure all conversions to be fallible `-> Result<_>`

--- src/types/norm/monster_card.rs
+++ src/types/norm/monster_card.rs
@@ -32,7 +32,7 @@ pub struct MonsterCard
 
 impl TryFrom<CardData> for MonsterCard
 {
-    type Error = &'static str;
+    type Error = Box<dyn std::error::Error>;
 
     fn try_from(data: CardData) -> Result<Self, Self::Error>
     {
@@ -44,19 +44,19 @@ impl TryFrom<CardData> for MonsterCard
             is_effect: data.is_effect > 0,
             effect:    data.effect,
 
-            monster_kind: monster::Kind::from(data.monster_color),
-            level:        monster::check_level(data.level),
-            attribute:    monster::Attribute::from(data.attribute),
-            monster_type: monster::Type::from(data.r#type),
-            ability:      monster::Ability::from_many(data.ability),
+            monster_kind: monster::Kind::try_from(data.monster_color)?,
+            level:        data.level,
+            attribute:    monster::Attribute::try_from(data.attribute)?,
+            monster_type: monster::Type::try_from(data.r#type)?,
+            ability:      monster::Ability::try_from_many(data.ability)?,
             flip:         data.flip > 0,
-            atk:          data.atk.parse().ok(),
-            def:          data.def.parse().ok(),
+            atk:          monster::try_parse_atk_def(data.atk)?,
+            def:          monster::try_parse_atk_def(data.def)?,
             is_pend:      data.pendulum > 0,
             pend_scale:   data.scale,
             pend_effect:  data.pendulum_effect,
 
-            link_arrows:  monster::LinkArrow::from_many(data.arrows),
+            link_arrows:  monster::LinkArrow::try_from_many(data.arrows)?,
 
             limit:        data.tcg_limit,
         })
@@ -68,124 +68,144 @@ pub use monster::*;
 
 mod monster
 {
-    #[derive(std::fmt::Debug)]
-    pub enum Kind {
-        NORMAL, EFFECT, RITUAL,
-        FUSION, SYNCHRO, XYZ, LINK,
-    }
 
-    impl From<String> for Kind {
-        fn from(value: String) -> Self {
-            match value.as_str() {
-                "Normal"  => Self::NORMAL,
-                "Effect"  => Self::EFFECT,
-                "Ritual"  => Self::RITUAL,
-                "Fusion"  => Self::FUSION,
-                "Synchro" => Self::SYNCHRO,
-                "Xyz"     => Self::XYZ,
-                "Link"    => Self::LINK,
-                _ => panic!("Received invalid Monster Card Type: {value}"),
-            }
+#[derive(std::fmt::Debug)]
+pub enum Kind {
+    NORMAL, EFFECT, RITUAL,
+    FUSION, SYNCHRO, XYZ, LINK,
+}
+
+impl TryFrom<String> for Kind {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.as_str() {
+            "Normal"  => Ok(Self::NORMAL),
+            "Effect"  => Ok(Self::EFFECT),
+            "Ritual"  => Ok(Self::RITUAL),
+            "Fusion"  => Ok(Self::FUSION),
+            "Synchro" => Ok(Self::SYNCHRO),
+            "Xyz"     => Ok(Self::XYZ),
+            "Link"    => Ok(Self::LINK),
+            _ => Err(format!("Received invalid Monster Card Type: {value}")),
         }
     }
+}
 
 
-    pub fn check_level(level: u8) -> u8
-    {
-        assert!(level >= 0, "Received negative Monster Level: {level}");
-        level
-    }
+#[derive(std::fmt::Debug)]
+pub enum Attribute {
+    LIGHT, DARK, WATER, FIRE, EARTH, WIND, DIVINE,
+}
 
+impl TryFrom<String> for Attribute {
+    type Error = String;
 
-    #[derive(std::fmt::Debug)]
-    pub enum Attribute {
-        LIGHT, DARK, WATER, FIRE, EARTH, WIND, DIVINE
-    }
-
-    impl From<String> for Attribute {
-        fn from(value: String) -> Self {
-            match value.as_str() {
-                "LIGHT" => Self::LIGHT,
-                "DARK"  => Self::DARK,
-                "WATER" => Self::DARK,
-                "FIRE"  => Self::FIRE,
-                "EARTH" => Self::EARTH,
-                "WIND"  => Self::WIND,
-                _ => panic!("Received invalid Monster Attribute: {value}"),
-            }
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.as_str() {
+            "LIGHT" => Ok(Self::LIGHT),
+            "DARK"  => Ok(Self::DARK),
+            "WATER" => Ok(Self::DARK),
+            "FIRE"  => Ok(Self::FIRE),
+            "EARTH" => Ok(Self::EARTH),
+            "WIND"  => Ok(Self::WIND),
+            _ => Err(format!("Received invalid Monster Attribute: {value}")),
         }
     }
+}
 
 
-    #[derive(std::fmt::Debug)]
-    pub enum Type {
-        CYBERSE, DRAGON, MACHINE,
-    }
+#[derive(std::fmt::Debug)]
+pub enum Type {
+    CYBERSE, DRAGON, MACHINE,
+}
 
-    impl From<String> for Type {
-        fn from(value: String) -> Self {
-            match value.to_ascii_lowercase().as_str() {
-                "cyberse" => Self::CYBERSE,
-                "dragon"  => Self::DRAGON,
-                "machine" => Self::MACHINE,
-                _ => panic!("Received invalid Monster Type: {value}"),
-            }
+impl TryFrom<String> for Type {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_ascii_lowercase().as_str() {
+            "cyberse" => Ok(Self::CYBERSE),
+            "dragon"  => Ok(Self::DRAGON),
+            "machine" => Ok(Self::MACHINE),
+            _ => Err(format!("Received invalid Monster Type: {value}")),
         }
     }
+}
 
 
-    #[derive(std::fmt::Debug)]
-    pub enum Ability {
-        GEMINI, SPIRIT, TOON, TUNER, UNION,
+#[derive(std::fmt::Debug)]
+pub enum Ability {
+    GEMINI, SPIRIT, TOON, TUNER, UNION,
+}
+
+impl Ability {
+    pub fn try_from_many(value: String) -> Result<Vec<Self>, String> {
+        value.split(r#" \/ "#)
+            .map(|s| Self::try_from(s.to_string()))
+            .collect()
     }
+}
 
-    impl Ability {
-        pub fn from_many(value: String) -> Vec<Self> {
-            value.split(r#" \/ "#)
-                .map(|s| Self::from(s.to_string()))
-                .collect()
+impl TryFrom<String> for Ability {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.as_str() {
+            "Gemini" => Ok(Self::GEMINI),
+            "Spirit" => Ok(Self::SPIRIT),
+            "Toon"   => Ok(Self::TOON),
+            "Tuner"  => Ok(Self::TUNER),
+            "Union"  => Ok(Self::UNION),
+            _ => Err(format!("Received invalid Monster Ability: {value}")),
         }
     }
+}
 
-    impl From<String> for Ability {
-        fn from(value: String) -> Self {
-            match value.as_str() {
-                "Gemini" => Self::GEMINI,
-                "Spirit" => Self::SPIRIT,
-                "Toon"   => Self::TOON,
-                "Tuner"  => Self::TUNER,
-                "Union"  => Self::UNION,
-                _ => panic!("Received invalid Monster Ability: {value}"),
-            }
+
+pub fn try_parse_atk_def(value: String) -> anyhow::Result<Option<u16>>
+{
+    if value == "?" {
+        Ok(None)
+    } else {
+        Ok(Some(value.parse()?))
+    }
+}
+
+
+#[derive(std::fmt::Debug)]
+pub enum LinkArrow {
+    UP, DOWN, LEFT, RIGHT,
+    UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT,
+}
+
+impl LinkArrow {
+    pub fn try_from_many(value: String) -> Result<Vec<Self>, String> {
+        let out: Vec<Self> =
+            value
+            .chars()
+            .zip(LINK_ARROWS)
+            .filter(|(c, _)| *c == '1')
+            .map(|(_, arrow)| arrow)
+            .collect();
+
+        if out.len() != 8 {
+            Err(format!("Received unexpected number of Link Arrows: {}", out.len()))
+        } else {
+            Ok(out)
         }
     }
+}
 
+const LINK_ARROWS: [LinkArrow; 8] = [
+    LinkArrow::UP_LEFT,
+    LinkArrow::UP,
+    LinkArrow::UP_RIGHT,
+    LinkArrow::RIGHT,
+    LinkArrow::DOWN_RIGHT,
+    LinkArrow::DOWN,
+    LinkArrow::DOWN_LEFT,
+    LinkArrow::LEFT,
+];
 
-    #[derive(std::fmt::Debug)]
-    pub enum LinkArrow {
-        UP, DOWN, LEFT, RIGHT,
-        UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT,
-    }
-
-    impl LinkArrow {
-        pub fn from_many(value: String) -> Vec<Self> {
-            value.chars()
-                .enumerate()
-                .filter_map(|(i, c)| {
-                    if c != '1' { None }
-                    else { Some(match i {
-                        0 => Self::UP_LEFT,
-                        1 => Self::UP,
-                        2 => Self::UP_RIGHT,
-                        3 => Self::RIGHT,
-                        4 => Self::DOWN_RIGHT,
-                        5 => Self::DOWN,
-                        6 => Self::DOWN_LEFT,
-                        7 => Self::LEFT,
-                        _ => panic!("Received more than 8 Link Arrows"),
-                    }) }
-                })
-                .collect()
-        }
-    }
 }
